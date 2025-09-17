9b237371ce5524a6dbef0d7dfc4b6025d7c1cb87
Bayramali Dilek <bayramalidilek@gmail.com>
2025-09-17 20:23:39 +0200

    add blur to guide

--- app/page.tsx
+++ app/page.tsx
@@ -42,11 +42,11 @@ export default function Home() {
           <div>
             <div className="my-25 flex justify-around">
               <div className="flex flex-col xl:flex-row min-w-full space-x-10 space-y-10">
-                <div className="w-full xl:w-1/2 glass max-h-min">
+                <div className="w-full xl:w-1/2 glass max-h-min backdrop-blur backdrop-saturate-300">
                   <EnhancedTokenChart />
                 </div>
 
-                <div className="flex justify-center text-center w-full xl:w-1/2 glass">
+                <div className="flex justify-center text-center w-full xl:w-1/2 glass backdrop-blur backdrop-saturate-300">
                   <div className="flex text-center">
                     <RaydiumBuyingGuide />
                   </div>
